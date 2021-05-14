93dbc1bcba210ba5906512650e9230a15325b9dd
dmiller89 <davelm89@gmail.com>
2021-05-13 20:08:43 -0400

    Update fetch to Unsplash API

--- infinite-photo-gallery/script.js
+++ infinite-photo-gallery/script.js
@@ -1,9 +1,21 @@
 "use strict";
 
+// Configure Unsplash API
 const count = 10;
+const apiKey = "6IVx3zhUz-X0U4QaGGBqzdhYfMiftJPgvmPLbTCvN7s";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-const apiKey = "";
+// Get photos from Unsplash API
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } 
+    catch(error) {
+        // If there was an error
+    }
+}
 
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY&count=${count}`;
-
-
+// On load
+getPhotos();
