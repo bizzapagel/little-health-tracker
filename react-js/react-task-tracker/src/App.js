251232bdb31a86243303223065cd8b6d6bee4642
dmiller89 <davelm89@gmail.com>
2021-05-21 07:57:22 -0400

    Added new projects

---
+++ react-js/react-task-tracker/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+	const name = 'Dave';
+
+	return (
+		<div className="container">
+			<h1>Hello, {name}!</h1>
+			<h2>Is this thing on?</h2>
+		</div>
+	);
+}
+
+export default App;
