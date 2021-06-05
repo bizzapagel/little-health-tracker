c2c9d4a87e1129dbc0d17b11c21aaa56516bab26
dmiller89 <davelm89@gmail.com>
2021-06-05 14:16:07 -0400

    Update Light/Dark Mode

--- javascript/light-dark-mode/script.js
+++ javascript/light-dark-mode/script.js
@@ -1,3 +1,25 @@
 "use strict";
 
+// Set toggle element
+ const toggleSwitch = document.querySelector("input[type='checkbox']");
+ const header = document.querySelector("#header");
+ const image1 = document.querySelector("#image1");
+ const image2 = document.querySelector("#image2");
+ const image3 = document.querySelector("#image3");
+ const textBox = document.querySelector(".project-textbox");
+
+// Dynamically change the theme
+function switchTheme(event) {
+    if(event.target.checked) {
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+    }
+}
+
+// Event listener
+toggleSwitch.addEventListener("change", switchTheme);
+
+
+
 
