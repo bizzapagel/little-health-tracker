a8149d7f0a8f09e47cd1dfab4efa2f0c13c0706f
dmiller89 <davelm89@gmail.com>
2021-06-18 10:44:39 -0400

    Update Little Blog

--- react-js/little-blog/src/Home.js
+++ react-js/little-blog/src/Home.js
@@ -1,10 +1,20 @@
+import {useState} from 'react';
+import PostList from './PostList.js';
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    );
+  const [posts, setPosts] = useState([
+    { title: 'My first post post', body: 'Lorem ipsum...', author: 'Frank', id: 1 },
+    { title: 'My second post post', body: 'Lorem ipsum...', author: 'Carl', id: 2 },
+    { title: 'My third post post', body: 'Lorem ipsum...', author: 'Pecky', id: 3 }
+  ]);
+
+  return (
+    <div className="home">
+      <PostList title="All Posts" posts={posts} />
+      <PostList title="Frank's Posts" posts={posts.filter((post) => post.author === 'Frank')} />
+    </div>
+  );
 }
- 
+
 export default Home;
 
