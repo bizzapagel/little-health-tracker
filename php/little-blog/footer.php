22623ff519bb7a00bd0eebbe440253a4f6e21249
dmiller89 <davelm89@gmail.com>
2021-07-18 18:08:56 -0400

    Update Little Blog (PHP)

---
+++ php/little-blog/footer.php
@@ -0,0 +1,10 @@
+    <div id="footer">
+      <div class="container">
+        Footer
+      </div>
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
--- php/little-blog/index.php
+++ php/little-blog/index.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Little Blog</title>
-</head>
-<body>
+<?php require('header.php'); ?>
+
+<div id="content">
+
   <?php
-    define("NAME", "Davey");
+    define('NAME', 'Davey');
     $age = 32;
   ?>
 
-  <?php
-    echo NAME . " is my name.";
-  ?>
+  <div class="container">
+    <p><?php echo 'Hello, my name is ' . NAME; ?></p>
+  </div>
+    
+</div>
 
-</body>
-</html>
-
+<?php require('footer.php'); ?>
