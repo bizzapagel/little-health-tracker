2e866159a64364c78a7d9a007882f38880ad22f8
dmiller89 <davelm89@gmail.com>
2021-07-23 13:14:19 -0400

    Create Fruitpedia project

---
+++ Fruitpedia/Fruitpedia/FruitpediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitpediaApp.swift
+//  Fruitpedia
+//
+//  Created by Dave Miller on 7/23/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitpediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
