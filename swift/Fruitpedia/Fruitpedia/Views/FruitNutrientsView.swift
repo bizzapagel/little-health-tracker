51900effcb31d22367997196a00f38659dcb8d0e
dmiller89 <davelm89@gmail.com>
2021-07-28 21:30:20 -0400

    Update Fruitpedia

---
+++ swift/Fruitpedia/Fruitpedia/Views/FruitNutrientsView.swift
@@ -0,0 +1,57 @@
+//
+//  FruitNutrientsView.swift
+//  Fruitpedia
+//
+//  Created by Dave Miller on 7/28/21.
+//
+
+import SwiftUI
+
+struct FruitNutrientsView: View {
+	
+	// MARK: - Properties
+	
+	var fruit: Fruit
+	let nutrients: [String] = [
+		"Energy",
+		"Sugar",
+		"Far",
+		"Protein",
+		"Vitamins",
+		"Minerals"
+	]
+	
+	// MARK: - Body
+	
+    var body: some View {
+		GroupBox() {
+			DisclosureGroup("Nutritional value per 100g") {
+				ForEach(0..<nutrients.count, id: \.self) { item in
+					Divider().padding(.vertical, 2)
+					HStack {
+						Group {
+							Image(systemName: "info.circle")
+							Text(nutrients[item])
+						}
+						.foregroundColor(fruit.gradientColors[1])
+						.font(Font.system(.body).bold())
+						Spacer(minLength: 25)
+						Text(fruit.nutrition[item])
+							.multilineTextAlignment(.trailing)
+					}
+				}
+			}
+		}
+    }
+}
+
+// MARK: - Preview
+
+struct FruitNutrientsView_Previews: PreviewProvider {
+    static var previews: some View {
+		FruitNutrientsView(fruit: fruitData[0])
+			.preferredColorScheme(.dark)
+			.previewLayout(.fixed(width: 375, height: 480))
+			.padding()
+    }
+}
