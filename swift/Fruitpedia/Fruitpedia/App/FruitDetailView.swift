3be22831451fa41d9427ed1013c9a94f042bb2d2
dmiller89 <davelm89@gmail.com>
2021-07-28 21:32:56 -0400

    Update Fruitpedia

--- swift/Fruitpedia/Fruitpedia/App/FruitDetailView.swift
+++ swift/Fruitpedia/Fruitpedia/App/FruitDetailView.swift
@@ -33,6 +33,7 @@ struct FruitDetailView: View {
 							.font(.headline)
 							.multilineTextAlignment(.leading)
 						// Nutrients
+						FruitNutrientsView(fruit: fruit)
 						// Subheadline
 						Text("Learn more about \(fruit.title)".uppercased())
 							.fontWeight(.bold)
