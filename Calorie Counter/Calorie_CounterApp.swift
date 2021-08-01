72f9292288feccb45ea95ccaf49d1b46e32ad226
dmiller89 <davelm89@gmail.com>
2021-08-01 09:53:22 -0400

    Setting things up

---
+++ Calorie Counter/Calorie_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_CounterApp.swift
+//  Calorie Counter
+//
+//  Created by Dave Miller on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Calorie_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
