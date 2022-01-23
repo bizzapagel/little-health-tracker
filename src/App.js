e37eabb90427794100019f2f0009e1731c7c8c76
dmiller89 <davelm89@gmail.com>
2022-01-23 14:48:37 -0500

    Remove stuff I don't need yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [totalCalories, setTotalCalories] = useState(0);
+  const [maxCalories, setmaxCalories] = useState(2000);
+  const [meals, setMeals] = useState([
+    {title: 'Pizza', id: 1},
+    {title: 'Cake', id: 2},
+    {title: 'Ice Cream', id: 3}
+  ]);
+
+  const handleAddCalories = () => {
+    setTotalCalories(prevState => prevState + 200);
+  };
+
+  const handleAddMeal = () => {
+    setMeals((prevMeals) => {
+      return prevMeals.concat(
+        {title: 'Dong', id: 4}
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Calorie Counter</h1>
+      <p>Calories: {totalCalories} / {maxCalories}</p>
+
+      <div>
+        {meals.map((meal) => (
+          <div key={meal.id}>{meal.title}</div>
+        ))}
+      </div>
+      
+      <div>
+        <button onClick={handleAddCalories}>Add Calories</button>
+      </div>
+
+      <div>
+        {/* <button onClick={() => {handleAddMeal()}}>Add Meal</button> */}
+        <button onClick={handleAddMeal}>Add Meal</button>
+      </div>
+
     </div>
   );
 }
